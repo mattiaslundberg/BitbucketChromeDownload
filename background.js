8b4f11a3ef9fe31b9728f2392685f869a6f0fd67
Mattias Lundberg <lundberg.mattias@gmail.com>
2014-04-10 19:34:27 +0200

    Fix show of page action and cactually download the patch file.

--- background.js
+++ background.js
@@ -3,10 +3,21 @@ chrome.runtime.onInstalled.addListener(function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [
 				new chrome.declarativeContent.PageStateMatcher({
-					pageUrl: { hostEquals: 'bitbucket.org' },
+					pageUrl: { hostEquals: "bitbucket.org", pathContains: "pull-request/" },
 				})
 			],
-			actions: [ function() { alert("test")} ]
+			actions: [ new chrome.declarativeContent.ShowPageAction() ]
 		}]);
 	});
+});
+
+chrome.pageAction.onClicked.addListener(function(tab){
+	var params = tab.url.split("/"),
+		org = params[3],
+		repo = params[4],
+		num = params[6];
+
+	var url = "https://bitbucket.org/api/2.0/repositories/" + org + "/" + repo + "/pullrequests/" + num + "/patch";
+	var name = "bitbucket-" + org + "-" + repo + "-pullrequest-" + num + ".patch";
+	chrome.downloads.download({url: url, filename: name, conflictAction: "overwrite"}, function (id) {});
 });
