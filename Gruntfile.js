cbf58cd08fb6a77c0bd6a85f6b0be2d6d54599be
Mattias Lundberg <lundberg.mattias@gmail.com>
2014-04-28 20:20:01 +0200

    Add grunt jshint.

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jshint: {
+			uses_defaults: ['*.js'],
+		},
+	});
+	grunt.registerTask('default', ['jshint']);
+};
