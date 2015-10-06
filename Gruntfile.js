a7f7ea5961ed2803d3f15d4039ed5ed69a341913
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2015-10-06 18:58:33 +0200

    Some cleanup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,10 @@
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-shell');
-	grunt.loadNpmTasks('grunt-zip');
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			uses_defaults: ['*.js'],
 		},
-		uglify: {
-			release: {
-				files: {
-					'background.min.js': ['background.js'],
-					'content.min.js': ['content.js']
-				}
-			}
-		},
-		zip: {
-			'package.zip': ['background.js', 'content.js', 'manifest.json', 'images/*']
-		},
-		shell: {
-			preparezip: {
-				command: function() {
-					return [
-						'mv background.js background.js.bak',
-						'mv background.min.js background.js',
-						'mv content.js content.js.bak',
-						'mv content.min.js content.js',
-					].join('&&');
-				}
-			},
-			finalize: {
-				command: function() {
-					return 'mv background.js.bak background.js && mv content.js.bak content.js';
-				}
-			}
-		}
 	});
-	grunt.registerTask('default', ['jshint', 'uglify', 'shell:preparezip', 'zip', 'shell:finalize']);
+	grunt.registerTask('default', ['jshint']);
 };
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[![Build Status](https://drone.io/bitbucket.org/mattiaslundberg/bitbucket_chrome_download/status.png)](https://drone.io/bitbucket.org/mattiaslundberg/bitbucket_chrome_download/latest)
-
 Avaliable at [Chrome Web Store](https://chrome.google.com/webstore/detail/bitbucket-patch-download/kmjcnbdahgjagapjigblfpkggohhceka).
 
 Download Bitbucket pull-request patches as a git patch. This makes it possible to locally check the changes that are correct without merging the pull request.
